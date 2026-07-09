b28b88ed7a480d1e988d179597a1d4a099865093
Joao <joao.m.junior7@docente.senai.br>
2026-07-09 20:05:20 -0300

    Criando repositório para as aulas

---
+++ src/SegundaAula.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SegundaAula {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        int idade = entrada.nextInt();
+        entrada.nextLine();
+
+        double altura = entrada.nextDouble();
+        entrada.nextLine();
+    }
+}
