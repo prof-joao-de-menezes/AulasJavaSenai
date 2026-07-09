dabce1934396c58a1767f922adb7652036ee9cf6
Joao <joao.m.junior7@docente.senai.br>
2026-07-09 20:16:28 -0300

    Configurando segunda aula

--- src/SegundaAula.java
+++ src/SegundaAula.java
@@ -1,13 +1,2 @@
-import java.util.Scanner;
-
 public class SegundaAula {
-    public static void main(String[] args){
-        Scanner entrada = new Scanner(System.in);
-
-        int idade = entrada.nextInt();
-        entrada.nextLine();
-
-        double altura = entrada.nextDouble();
-        entrada.nextLine();
-    }
 }
