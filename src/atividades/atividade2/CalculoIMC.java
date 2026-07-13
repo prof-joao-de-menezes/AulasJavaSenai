a1d3132d9f2f70054a8399103011331f385bb1ad
Joao <joao.m.junior7@docente.senai.br>
2026-07-13 18:29:00 -0300

    Adicionando Scanner dentro do CalculoIMC.java

--- src/atividades/atividade2/CalculoIMC.java
+++ src/atividades/atividade2/CalculoIMC.java
@@ -1,7 +1,18 @@
 package atividades.atividade2;
 
+import java.util.Scanner;
+
 public class CalculoIMC {
     public static void main(String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
+
+        int valor = entrada.nextInt();
+
+        if (valor < 10 && valor > 0 || valor == 5) {
+            System.out.println("Verdade");
+        } else {
+            System.out.println("Mentira");
+        }
     }
 }
